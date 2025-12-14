253494f7b8d9f6843bd7d4a0582a01121cc78a90
rehankhan88 <rehan9732088@gmail.com>
2025-12-13 21:01:20 -0800

    add inbound rule and outbound rule

--- ec2.tf
+++ ec2.tf
@@ -19,12 +19,41 @@ resource "aws_security_group" "my_scurity_group" {
     # inbound rule
     ingress {
         from_port = 22
-        to_prot
+        to_prot = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "ssh open"
+    }
+    ingress {
+        from_port = 80
+        to_prot = 80
+        protoclo = "tcp"
+        cidr_blocks = ["0.0.0.0/0]
+        description = "http open"
+    }
+    ingress {
+        from_port = 443
+        to_prot = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "http open"
+    }
+    ingress {
+        from_port = 8000
+        to_port = 8000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "django open"
     }
 
 
     # outbound rule
     egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "all traffic allowed"
 
     
     }
